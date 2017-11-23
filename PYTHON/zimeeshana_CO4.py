477a9f02899f6e3d95bccb75836340ea0c730f47
Guest <erkinnnn@inbox.lv>
2017-11-23 17:17:56 +0200

    DMI

---
+++ PYTHON/zimeeshana_CO4.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4])
+plt.plot([-2,0,1],[4,0,-3],'*', markerfacecolor='black', markersize=40)
+                    
+plt.xlabel('some other numbers')
+plt.ylabel('some numbers')
+
+
+plt.show()
