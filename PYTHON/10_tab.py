73146da7cc1d8d0325c04167ded88cc107dc1d86
saulespuke101@gmail.com <erkinnnn@inbox.lv>
2018-02-01 17:25:55 +0200

    DMI

---
+++ PYTHON/10_tab.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+x=input ("ievadi x: ")
+print"x= ", (x)
+print "x  || \t%1 | \t%2 | \t%3 | \t%4 | \t%5 | \t%10"
+print "------------------------------------------------------"
+z=0
+while (z < 11):
+     a= x%1
+     b= x%2
+     c= x%3
+     d= x%4
+     e= x%5
+     f= x%10
+     #print (x), "|\t %5d\t %5d\t %5d\t %5d\t %5d0\t %5d\t" # (a), "|\t" , (b), "|\t" , (c), "|\t" , (d), "|\t", (e), "|\t", (f)
+     print "%3d |%5d%7d%8d%9d%8d%8d"%(x,a,b,c,d,e,f)
+     z = z+1
+     x = x+1
+     
